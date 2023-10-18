fe657b48bc75b6bfe1cce93567f60666f9bb8a46
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-10-18 13:45:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -357,7 +357,7 @@ function uiAdjustVKLayout() {
     vkMap['r'].style = makeVKStyle(offTop, window.innerWidth - vkw, vkw, vkh, fontSize)
     $id('vk-menu').style = makeVKStyle(offTop, window.innerWidth / 2 - vkw / 2, vkw, vkh, fontSize);
     $id('vk-menu').style.position = 'fixed';
-    $id('vk-menu').style.bottom = '0';
+    $id('vk-menu').style.bottom = '10';
     $id('vk-menu').style.left = '50%';
     $id('vk-menu').style.transform = 'translateX(-50%)';
 
