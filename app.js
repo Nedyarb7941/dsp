7ddc827e917d62076d2825fcf0080830d273f010
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-10-26 12:27:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -360,7 +360,7 @@ function uiAdjustVKLayout() {
     $id('vk-menu').style.position = 'fixed';
     $id('vk-menu').style.transform = 'scale(0.65)';
     $id('vk-menu').style.left = '50%';
-    $id('vk-menu').style.transform += ' translateX(-81.75%)';
+    $id('vk-menu').style.transform += ' translateX(-80.75%)';
 
 
     offTop += baseSize * 0.62
