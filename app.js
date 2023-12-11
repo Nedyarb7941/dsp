12e322c677da2c657da83ee25b267bcd6ad53c01
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-12-11 11:24:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -435,8 +435,8 @@ function uiSwitchTo(mode) {
     emuIsRunning = false
 
     if (mode == 'player') {
-        body.style = 'touch-action: none;'
-        html.style = 'background: black;position: fixed;overflow:hidden;touch-action: none;'
+        body.style = 'background: black;touch-action: none;'
+        html.style = 'position: fixed;overflow:hidden;touch-action: none;'
         for (var i = 0; i < 14; i++) {
             emuKeyState[i] = false
         }
